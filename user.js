d751f49340589bd9d88a0f49d37b3696b0cbd175
Samuel FORESTIER <dev@samuel.domains>
2020-11-09 14:12:06 +0100

    Copyright/authors cleanup (see privacytools/privacytools.io#2120)

--- user.js
+++ user.js
@@ -2,7 +2,6 @@
 * name: thunderbird user.js
 * date: 1 November 2020
 * version: v78-beta4
-* authors: v52+ github | v51- www.ghacks.net
 * url: https://github.com/HorlogeSkynet/thunderbird-user.js
 * license: MIT (https://github.com/HorlogeSkynet/thunderbird-user.js/blob/master/LICENSE)
 * releases: https://github.com/HorlogeSkynet/thunderbird-user.js/releases
