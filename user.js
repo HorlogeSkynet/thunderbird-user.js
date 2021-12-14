de8048fea63b24e26e020a48cdb139c6c7ff7696
Samuel FORESTIER <dev@samuel.domains>
2021-12-14 14:24:05 +0100

    Don't clear `mail.collect_addressbook` pref (feature already disabled)
    > see #17

--- user.js
+++ user.js
@@ -1523,7 +1523,7 @@ user_pref("mailnews.display.date_senders_timezone", false);
  * Consider using CardBook extension instead (https://addons.thunderbird.net/addon/cardbook/)
  * [SETTING] Preferences>Composition>Addressing>Automatically add outgoing e-mail addresses...
  * [SETTING][CARDBOOK] CardBook>Preferences>Email>Collect Outgoing Email ***/
-user_pref("mail.collect_addressbook", "");  // [DEFAULT: "jsaddrbook://history.sqlite"]
+   // user_pref("mail.collect_addressbook", "jsaddrbook://history.sqlite");
 user_pref("mail.collect_email_address_outgoing", false);
 /* 6031: Only use email addresses, without their Display Names [CARDBOOK] [SETUP-FEATURE]
  * By default, CardBook extension incorporates contacts display names in addresses fields.
