dcf351aa96bc1be35dc85faacfb06f53d0af8eb6
Samuel FORESTIER <dev@samuel.domains>
2021-12-14 14:24:28 +0100

    [META] Bumps to v78.1

--- user.js
+++ user.js
@@ -1,7 +1,7 @@
 /******
 * name: thunderbird user.js
-* date: 28 August 2021
-* version: v78.0
+* date: 14 December 2021
+* version: v78.1
 * url: https://github.com/HorlogeSkynet/thunderbird-user.js
 * license: MIT (https://github.com/HorlogeSkynet/thunderbird-user.js/blob/master/LICENSE)
 * releases: https://github.com/HorlogeSkynet/thunderbird-user.js/releases
