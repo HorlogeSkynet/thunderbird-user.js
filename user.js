a1741217821541920babfe47e06c279b79eed1ed
boredsquirrel <113100745+boredsquirrel@users.noreply.github.com>
2024-05-13 19:37:41 +0000

    enable project notes and donation appeals
    This Mail program was literally dead. Keeping these "debloating" settings may have a sense, but there is no security impact.
    
    it is as opinionated as Arkenfox. Disabling any update notifications or donation appeals has no privacy benefit and comes from a really toxic "free as in free beer" perspective.
    
    I would be happy to comment these lines out, also because it may be interesting for users to get news from Thunderbird

--- user.js
+++ user.js
@@ -82,7 +82,7 @@ user_pref("browser.aboutConfig.showWarning", false);
 user_pref("_user.js.parrot", "0100 syntax error: the parrot's dead!");
 /* 0102: set START page [SETUP-CHROME]
  * [SETTING] General > Thunderbird Start Page ***/
-user_pref("mailnews.start_page.enabled", false);
+// user_pref("mailnews.start_page.enabled", false);
 /* 0104: set NEWTAB page
  * true=? (default), false=blank page ***/
 user_pref("browser.newtabpage.enabled", false);
@@ -202,7 +202,7 @@ user_pref("mail.rights.override", true);  // [DEFAULT: unset]
  * $url Web page is opened if $viewed is lower than $version (and 0330 policy bypass notification is disabled)
  * [1] https://searchfox.org/comm-esr115/source/mail/base/content/messenger.js#455 ***/
    // user_pref("app.donation.eoy.version", 2);
-user_pref("app.donation.eoy.version.viewed", 999);
+// user_pref("app.donation.eoy.version.viewed", 999);
    // user_pref("app.donation.eoy.url", "https://www.thunderbird.net/thunderbird/115.0/appeal/");
 /* 0380: disable the new/unread message count badge on taskbar icon
  * [1] https://www.thunderbird.net/en-US/thunderbird/91.0.2/releasenotes/#whatsnew */
